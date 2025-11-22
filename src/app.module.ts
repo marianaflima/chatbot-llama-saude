00a950221821ce5f26278b090b22d30945202529
marianaflima <marianafelix.dev@gmail.com>
2025-11-22 20:17:54 -0300

    fix: removing comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ChatModule } from './chat/chat.module';
 import { ConfigModule } from '@nestjs/config';
-import { join } from 'path';
-// import { ChatbotService } from './chatbot/chatbot.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), ChatModule],
-  // providers: [ChatbotService],
 })
 export class AppModule {}
