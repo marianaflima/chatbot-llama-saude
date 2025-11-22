72ef30ffdf87c4f00eead4d39d36e163296e4367
marianaflima <marianafelix.dev@gmail.com>
2025-11-22 20:14:56 -0300

    fix: remove ChatService from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ChatModule } from './chat/chat.module';
 import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
+// import { ChatbotService } from './chatbot/chatbot.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), ChatModule],
+  // providers: [ChatbotService],
 })
 export class AppModule {}
